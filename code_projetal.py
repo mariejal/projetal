cbbb731aad6556ca97f2f8afd76f0e782825f63c
mariejal <mariefrancejallade@gmail.com>
2019-04-27 23:14:45 +0200

    debut

--- code_projetal.py
+++ code_projetal.py
@@ -1,8 +1,16 @@
 import os
 from collections import defaultdict
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("verb_chosen", help = "verbe à clusteriser", default = None)
+args = parser.args()
 
 class Desamb:
 
-	def __init__(self):
+	def __init__(self, verb_chosen):
 
+		self.verb_chosen = verb_chosen
 		self.
+
+
